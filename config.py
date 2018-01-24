c70075f962b400aff5924c774a2b537015f1fa44
AleksBelytskyi <aleksandr@serverius.net>
2018-01-24 17:32:20 +0100

    Error Handling

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ class Config(object):
     SECRET_KEY = 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = None
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 0
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+    ADMINS = ['belitckiu.lex@gmail.com']
